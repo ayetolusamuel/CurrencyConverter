5fb06dc5a2626757c084fda221e5a766a8860bac
ayetolusamuel <ayetolusamuel@gmail.com>
2018-05-19 07:24:36 +0100

    Initial commit

---
+++ app/src/main/java/com/setnumd/technologies/currencyconverter/MainActivity.java
@@ -0,0 +1,46 @@
+package com.setnumd.technologies.currencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final double NAIRA_CONVERSION_RATE = 360.50;
+    private EditText amountToBeConverted;
+private TextView resultTextView;
+private Button convertButton;
+private ImageView imageView;
+private static final String TAG = MainActivity.class.getSimpleName();
+    public void convertButtonClick(View view){
+String amountValue = amountToBeConverted.getText().toString();
+if (amountValue.length()!= 0){
+    double amountValueInDouble  = Double.parseDouble(amountValue);
+    double result = amountValueInDouble*NAIRA_CONVERSION_RATE;
+    resultTextView.setText("₦"+String.valueOf(result));
+
+}
+else{
+    Toast.makeText(MainActivity.this,"Enter amount to be converted to ₦",Toast.LENGTH_SHORT).show();
+}
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        amountToBeConverted = findViewById(R.id.edt_amount);
+        resultTextView = findViewById(R.id.resultTextView);
+        convertButton = findViewById(R.id.convertButton);
+
+
+    }
+}
